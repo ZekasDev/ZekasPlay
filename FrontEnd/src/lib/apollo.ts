4249e69f290d3e599041d898a3b05702418a73f3
ZekasDev <dannndourado@gmail.com>
2023-02-23 15:58:11 -0300

    Primeira Etapa

--- FrontEnd/src/lib/apollo.ts
+++ FrontEnd/src/lib/apollo.ts
@@ -1,5 +1,6 @@
-import { ApolloClient } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-
+    uri: "https://api-sa-east-1.hygraph.com/v2/cle37c4qw0ekg01ur6x049p6y/master",
+    cache: new InMemoryCache()
 })
