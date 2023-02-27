958e817de3878ba0de855b6c580c10c84fb81ab7
ZekasDev <dannndourado@gmail.com>
2023-02-27 16:53:44 -0300

    Iniciando Projeto

---
+++ FrontEnd/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <header className="w-full py-5 flex itens-center justify-center bg-gray-700 border-b border-gray-600">
+            <h1>Zekas<span>Play</span>|<p>ReactJS</p></h1>
+        </header>
+    )
+}
